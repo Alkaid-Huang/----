de1dcfd1dc6f20e7fa5badce3aeb82772547fe63
Alkaid-Huang <2602539653@qq.com>
2026-05-03 00:16:10 +0800

    开始做第二关之前

---
+++ scenes/components/interactable_component.gd
@@ -0,0 +1,14 @@
+extends Area2D
+class_name InteractableComponent
+
+signal interactable_activated
+signal interactable_deactivated
+
+func _on_body_entered(body: Node2D) -> void:
+	interactable_activated.emit()
+	print("进入交互")
+
+
+func _on_body_exited(body: Node2D) -> void:
+	interactable_deactivated.emit()
+	print("离开交互")
